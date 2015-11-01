7f7c2460dc3ea7e90bf1a11db1fda94ed7028f99
Álvaro Baño Fos <alvarobanofos@gmail.com>
2015-11-01 04:01:14 +0100

    comprobacion DB

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,54 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var confDB = {
+    initialize: function() {
+
+        var existe_db;
+        // Comprobacion de la existencia de la base de datos
+        existe_db = window.localStorage.getItem("existe_db");
+
+
+        if (typeof existe_db == 'undefined' || existe_db == null)
+            this.confirmaCreacionBBDD()
+    },
+
+    //lanza el dialogs de confirmación y retorna su resultado
+    confirmaCreacionBBDD:function() {
+        var confirm = false;
+        navigator.notification.confirm(
+            'La base de datos no existe.\n' +
+            '¿Desea crear una nueva?',
+            this.onConfirm,
+            'Base de datos',
+            ['Crear', 'Salir']
+        );
+
+
+    },
+
+    //genera la base de datos
+    creaBBDD:function() {
+        //Simulamos generación de base de datos
+        window.localStorage.setItem("existe_db", 1);
+
+
+    },
+
+    //Maneja el resultado del dialogo de confirmación.
+    //En caso de darle a salir sale de la aplicación.
+    onConfirm:function(confirm) {
+        if(confirm == 1) {
+            confDB.creaBBDD();
+        }
+        else navigator.app.exitApp();
+
+    }
+
+
+};
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -27,6 +75,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+
     },
     // deviceready Event Handler
     //
@@ -34,17 +83,12 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        confDB.initialize();
     }
 };
 
